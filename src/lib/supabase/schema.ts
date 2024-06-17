401a89aa3812c563a2daa6281f52ad27d7b1ced1
tianangthang7@gmail.com <tianangthang7@gmail.com>
2024-06-18 01:30:54 +0700

    init db

---
+++ src/lib/supabase/schema.ts
@@ -0,0 +1,23 @@
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import * as dotenv from 'dotenv'
+import * as schema from '../../../migrations/schema'
+import { migrate } from 'drizzle-orm/postgres-js/migrator'
+
+dotenv.config({ path: '.env' })
+if (!process.env.DATABASE_URL) {
+    console.log('No database url')
+}
+const client = postgres(process.env.DATABASE_URL as string, { max: 1 })
+const db = drizzle(client, { schema });
+const migrateDb = async () => {
+    try {
+        console.log('Migration start')
+        await migrate(db, { migrationsFolder: 'migrations' })
+        console.log('Migration end')
+    } catch (error) {
+        console.log('Error Migrating client')
+    }
+}
+migrateDb();
+export default db;
